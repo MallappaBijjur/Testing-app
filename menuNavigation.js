e90e2180d9ae9cd1aa6596effc411561c8f62df8
MallappaBijjur <Mallappabijjur80@gmail.com>
2018-01-04 05:48:48 -0600

    menu

---
+++ menuNavigation.js
@@ -0,0 +1,49 @@
+const menuNavigation = {
+  menuNavigationLeft: [
+    {
+      id: 'autoParts',
+      name: 'AUTO PARTS',
+      link: 'https://en.wikipedia.org/wiki/Mark_Huizinga'
+    },
+    {
+      id: 'Accessories',
+      name: 'ACCESSORIES',
+      link: 'https://en.wikipedia.org/wiki/Driulis_González'
+    },
+    {
+      id: 'Tools',
+      name: 'TOOLS',
+      link: 'https://en.wikipedia.org/wiki/Rishod_Sobirov'
+    },
+    {
+      id: 'OilandFluids',
+      name: 'OIL & FLIUDS',
+      link: 'https://en.wikipedia.org/wiki/Ryoko_Tani'
+    },
+    {
+      id: 'Performance',
+      name: 'PERFORMANCE',
+      link: 'https://en.wikipedia.org/wiki/Teddy_Riner'
+    },
+    {
+      id: 'Featured',
+      name: 'FEATURED',
+      link: 'https://en.wikipedia.org/wiki/Teddy_Riner'
+    }
+  ],
+
+  menuNavigationRight: [
+    {
+      id: 'Repairhelp',
+      name: 'Repair Help',
+      link: 'https://en.wikipedia.org/wiki/Teddy_Riner'
+    },
+    {
+      id: 'Deals',
+      name: 'Deals',
+      link: 'https://en.wikipedia.org/wiki/Teddy_Riner'
+    }
+  ]
+};
+
+export default menuNavigation;
